a17eadc38f4fc2be98e460ed78d02e788b66fd03
ArcOmotola <ogunsola40@gmail.com>
2021-11-21 01:48:36 +0100

    added portfolio page initial content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,40 @@ import "./portfolio.scss";
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
