4fe97e670a2a4301ca744437be2c59fe92dadcb8
ArcOmotola <ogunsola40@gmail.com>
2021-11-21 02:48:21 +0100

    added dynamic data with useEffect on portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import {featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio} from "../../data";
 
 export default function Portfolio() {
-    const [selected, setSelected] = useState("featured")
+    const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
     const list = [
         {
             id: "featured",
@@ -27,6 +29,28 @@ export default function Portfolio() {
         },
     ];
 
+    useEffect(() => {
+        switch(selected) {
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "mobile":
+                setData(mobilePortfolio);
+                break;
+            case "design":
+                setData(designPortfolio);
+                break;
+            case "content":
+                setData(contentPortfolio);
+                break;
+            default:
+                setData(featuredPortfolio);
+        }
+    }, [selected])
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
@@ -36,30 +60,12 @@ export default function Portfolio() {
                 ))}
             </ul>
             <div className="container">
-                <div className="item">
-                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://cdn.dribbble.com/users/2146089/screenshots/11479540/media/0d90b18030fe7a647fde330a9c1c7c5d.png?compress=1&resize=400x300" alt="" />
-                    <h3>Banking App</h3>
-                </div>
+                {data.map((d) => (
+                    <div className="item">
+                        <img src={d.img} alt="" />
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
             </div>
         </div>
     )
